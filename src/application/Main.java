40f8c1b44faf05c144d0dd1fd4efe7daffd88b4f
Vincent Huisman <vjhuisman@gmail.com>
2020-04-05 15:17:38 +0200

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("GameFramework");
         primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
     }
