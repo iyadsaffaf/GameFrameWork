f70237d60b93008ee8b34e44e22b7dbb278e786c
Ramon <ra.oosterloo@st.hanze.nl>
2020-04-12 22:57:53 +0200

    Singeplayer, Ai vs Ai

--- src/controller/ReversieController.java
+++ src/controller/ReversieController.java
@@ -96,7 +96,7 @@ public class ReversieController  {
                                         @Override
                                         public void run() {
                                             try {
-                                                Thread.sleep(1000);
+                                                Thread.sleep(2500);
                                             } catch (InterruptedException e) {
                                                 e.printStackTrace();
                                             }
